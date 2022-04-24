f1a7662b7b0b34de4bdf1bed9afad296d5b1f6f3
daaf <dfarr3@gmail.com>
2022-04-24 12:52:57 -0400

    remove route for post request

--- app.py
+++ app.py
@@ -11,9 +11,3 @@ app = flask_extensions.FlaskApp(__name__)
 @app.route("/philly-trash-holidays")
 def get_trash_holidays():
     return jsonify(globals.TRASH_HOLIDAYS_2022)
-
-
-@app.route("/philly-trash-holidays", methods=["POST"])
-def add_trash_holiday():
-    globals.TRASH_HOLIDAYS_2022.append(request.get_json())
-    return "", 204
