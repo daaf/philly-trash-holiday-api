9fb145ba44282b4c8cd7079eae3c422e7d96e903
daaf <dfarr3@gmail.com>
2022-04-24 15:43:40 -0400

    add docstrings and type annotations

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+"""Defines the Flask app and API endpoints."""
+
 # Third-party imports
-from flask import request, jsonify
+from flask import jsonify, Response
 
 # Local imports
 from . import flask_extensions, globals
@@ -9,5 +11,10 @@ app = flask_extensions.FlaskApp(__name__)
 
 
 @app.route("/philly-trash-holidays")
-def get_trash_holidays():
+def get_trash_holidays() -> Response:
+    """Get the list of trash holidays.
+
+    :return: The list of trash holidays serialized to JSON and wrapped
+        in a flask.Response object.
+    """
     return jsonify(globals.TRASH_HOLIDAYS_2022)
--- flask_extensions.py
+++ flask_extensions.py
@@ -1,3 +1,5 @@
+"""Classes to customize Flask functionality."""
+
 # Standard library imports
 from datetime import date
 
@@ -7,10 +9,18 @@ from flask.json import JSONEncoder
 
 
 class CustomJSONEncoder(JSONEncoder):
-    def default(self, o):
+    """JSONEncoder that encodes dates in ISO 8601 format.
+
+    Encoding dates in ISO 8601 format makes them serializable to JSON.
+    """
+
+    def default(self, o) -> str:
+        """If the provided object is a date, the date in ISO 8601 format."""
         if isinstance(o, date):
             return o.isoformat()
 
 
 class FlaskApp(Flask):
+    """Extends `Flask` by specifying a custom JSONEncoder."""
+
     json_encoder = CustomJSONEncoder
