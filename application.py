62b583ad01c049488df86a4fd1a61aebfc314ee5
daaf <dfarr3@gmail.com>
2022-04-24 22:18:03 -0400

    rename app.py -> application.py and implement limiter

--- application.py
+++ application.py
@@ -2,12 +2,19 @@
 
 # Third-party imports
 from flask import jsonify, Response
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 # Local imports
 from . import flask_extensions, constants
 
 
 app = flask_extensions.FlaskApp(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour", "10 per minute"],
+)
 
 
 @app.route("/philly-trash-holidays")
